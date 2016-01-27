374557aa9652db7330925d318cdc915f0aa67bd3
ship8-2 <ship8-2@ship8-2s-mac.local>
2016-01-27 21:17:14 +0100

    Initial Commit

---
+++ CleantheCreeks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CleantheCreeks
+//
+//  Created by ship8-2 on 1/27/16.
+//  Copyright © 2016 RedCherry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
