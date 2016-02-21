6fe8e6adce37125a18c04380109293a0bed51e37
Kimura Isoroku <kimuraisoroku@Kimuras-Mac.local>
2016-02-22 00:05:47 +0900

    Facebook Posting

--- CleantheCreeks/VCs/FacebookPostVC.h
+++ CleantheCreeks/VCs/FacebookPostVC.h
@@ -7,8 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Social/Social.h>
+#import <Accounts/Accounts.h>
 
 @interface FacebookPostVC : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *fbPostImg;
+@property (weak, nonatomic) IBOutlet UIImageView *fbTopImage;
 - (IBAction)skip:(id)sender;
-
+@property(strong,nonatomic)SLComposeViewController *mySLComposerSheet;
 @end
--- CleantheCreeks/VCs/FacebookPostVC.m
+++ CleantheCreeks/VCs/FacebookPostVC.m
@@ -20,4 +20,48 @@
 {
     [self.tabBarController setSelectedIndex:2];
 }
+- (IBAction)FBPost:(id)sender {
+    /*UIImage *bottomImage =fbPostImage.
+     UIImage *image       = [UIImage imageNamed:@"top.png"]; //foreground image
+     
+     CGSize newSize = CGSizeMake(width, height);
+     UIGraphicsBeginImageContext( newSize );
+     
+     // Use existing opacity as is
+     [bottomImage drawInRect:CGRectMake(0,0,newSize.width,newSize.height)];
+     
+     // Apply supplied opacity if applicable
+     [image drawInRect:CGRectMake(0,0,newSize.width,newSize.height) blendMode:kCGBlendModeNormal alpha:0.8];*/
+    
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    
+    UIGraphicsEndImageContext();
+    
+    
+    if([SLComposeViewController isAvailableForServiceType:SLServiceTypeFacebook]) //check if Facebook Account is linked
+    {
+        _mySLComposerSheet = [[SLComposeViewController alloc] init]; //initiate the Social Controller
+        _mySLComposerSheet = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook]; //Tell him with what social plattform to use it, e.g. facebook or twitter
+        [_mySLComposerSheet setInitialText:[NSString stringWithFormat:@"Test",_mySLComposerSheet.serviceType]]; //the message you want to post
+        [_mySLComposerSheet addImage:_fbPostImg.image]; //an image you could post
+        //for more instance methodes, go here:https://developer.apple.com/library/ios/#documentation/NetworkingInternet/Reference/SLComposeViewController_Class/Reference/Reference.html#//apple_ref/doc/uid/TP40012205
+        [self presentViewController:_mySLComposerSheet animated:YES completion:nil];
+    }
+    [_mySLComposerSheet setCompletionHandler:^(SLComposeViewControllerResult result) {
+        NSString *output;
+        switch (result) {
+            case SLComposeViewControllerResultCancelled:
+                output = @"Action Cancelled";
+                break;
+            case SLComposeViewControllerResultDone:
+                output = @"Post Successfull";
+                break;
+            default:
+                break;
+        } //check if everything worked properly. Give out a message on the state.
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Facebook" message:output delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+        [alert show];
+    }];
+
+}
 @end
