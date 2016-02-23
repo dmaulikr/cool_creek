5769f24a33740d88d8adf94cf3ecc11cbeb5876e
akio46 <yamadera46@hotmail.com>
2016-02-23 19:57:54 +0900

    SettingVC Storyboard

---
+++ CleantheCreeks/VCs/SettingVC.h
@@ -0,0 +1,13 @@
+//
+//  SettingVC.h
+//  Clean the Creek
+//
+//  Created by a on 2/23/16.
+//  Copyright © 2016 RedCherry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingVC : UIViewController
+
+@end
